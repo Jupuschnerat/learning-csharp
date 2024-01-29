531929cf25921da29a1d6677e8b64f2aca67f0ec
Julia Puschnerat <jupuschnerat@gmail.com>
2024-01-29 14:36:58 -0300

    commit

--- Program.cs
+++ Program.cs
@@ -42,6 +42,7 @@ namespace CursoCSharp {
                 // Classes e metodos
                 {"Membros - Estruturas De Controle", Membros.Executar},
                 {"Construtores - Estruturas De Controle", Construtores.Executar},
+                {"Metodos Com Retorno - Estruturas De Controle", MetodosComRetorno.Executar},
 
             });
 
